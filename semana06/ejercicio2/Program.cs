0c16134507fd5e630c94246d5333a65dde08ac49
LUISCUENCA15 <le.cuencaa@uea.edu.ec>
2025-07-02 13:56:01 -0500

    semana6
    semana6

---
+++ semana06/ejercicio2/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+class Nodo
+{
+    public int Dato;
+    public Nodo Siguiente;
+
+    public Nodo(int dato)
+    {
+        Dato = dato;
+        Siguiente = null;
+    }
+}
+
+class ListaEnlazada
+{
+    public Nodo Cabeza;
+
+    public void Agregar(int dato)
+    {
+        Nodo nuevo = new Nodo(dato);
+        nuevo.Siguiente = Cabeza;
+        Cabeza = nuevo;
+    }
+
+    public void Mostrar()
+    {
+        Nodo actual = Cabeza;
+        while (actual != null)
+        {
+            Console.Write(actual.Dato + " -> ");
+            actual = actual.Siguiente;
+        }
+        Console.WriteLine("null");
+    }
+
+    public void Invertir()
+    {
+        Nodo anterior = null;
+        Nodo actual = Cabeza;
+        Nodo siguiente = null;
+
+        while (actual != null)
+        {
+            siguiente = actual.Siguiente;
+            actual.Siguiente = anterior;
+            anterior = actual;
+            actual = siguiente;
+        }
+
+        Cabeza = anterior;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        ListaEnlazada lista = new ListaEnlazada();
+        lista.Agregar(10);
+        lista.Agregar(20);
+        lista.Agregar(30);
+        lista.Agregar(40);
+
+        Console.WriteLine("Lista original:");
+        lista.Mostrar();
+
+        lista.Invertir();
+
+        Console.WriteLine("Lista invertida:");
+        lista.Mostrar();
+    }
+}
+
---
+++ semana06/ejercicio3/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+class Nodo
+{
+    public int Dato;
+    public Nodo Siguiente;
+
+    public Nodo(int dato)
+    {
+        Dato = dato;
+        Siguiente = null;
+    }
+}
+
+class ListaEnlazada
+{
+    public Nodo Cabeza;
+
+    public void Agregar(int dato)
+    {
+        Nodo nuevo = new Nodo(dato);
+        nuevo.Siguiente = Cabeza;
+        Cabeza = nuevo;
+    }
+
+    public void Mostrar()
+    {
+        Nodo actual = Cabeza;
+        while (actual != null)
+        {
+            Console.Write(actual.Dato + " -> ");
+            actual = actual.Siguiente;
+        }
+        Console.WriteLine("null");
+    }
+
+    public void Buscar(int valor)
+    {
+        Nodo actual = Cabeza;
+        int contador = 0;
+
+        while (actual != null)
+        {
+            if (actual.Dato == valor)
+            {
+                contador++;
+            }
+            actual = actual.Siguiente;
+        }
+
+        if (contador > 0)
+        {
+            Console.WriteLine($"El valor {valor} se encuentra {contador} veces en la lista.");
+        }
+        else
+        {
+            Console.WriteLine($"El valor {valor} no fue encontrado en la lista.");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        ListaEnlazada lista = new ListaEnlazada();
+        lista.Agregar(5);
+        lista.Agregar(10);
+        lista.Agregar(15);
+        lista.Agregar(10);
+        lista.Agregar(20);
+
+        Console.WriteLine("Lista:");
+        lista.Mostrar();
+
+        Console.Write("Ingrese el valor que desea buscar: ");
+        int valor = int.Parse(Console.ReadLine());
+
+        lista.Buscar(valor);
+    }
+}
